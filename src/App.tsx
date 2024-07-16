fc2942eed911a23409c3443c06c45f14e9b9af85
biljeg <brunosiljeg@outlook.com>
2024-07-16 11:04:45 +0200

    feat: finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const shuffle = <T,>(arr: T[]): T[] => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
+const generateRandomColor = (): string => {
+  const hexDigits = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  const color = Array(6)
+    .fill("")
+    .map(() => hexDigits[Math.floor(Math.random() * hexDigits.length)])
+    .join("");
+
+  return color;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [correctColor, setCorrectColor] = useState<string>("");
+  const [availableColors, setAvailaibleColors] = useState<string[]>([]);
+  const [result, setResult] = useState<boolean | undefined>();
+
+  const pickColors = (): void => {
+    const newColor = generateRandomColor();
+    setCorrectColor(newColor);
+    const shuffledArray = shuffle([
+      newColor,
+      generateRandomColor(),
+      generateRandomColor(),
+    ]);
+    setAvailaibleColors(shuffledArray);
+  };
+
+  useEffect(() => {
+    pickColors();
+  }, []);
+
+  const handleClick = (pickedColor: string): void => {
+    if (pickedColor === correctColor) {
+      setResult(true);
+      pickColors();
+    } else {
+      setResult(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex flex-col items-center">
+      <div
+        className={`w-96 h-48 rounded-md border border-white`}
+        style={{ backgroundColor: correctColor }}
+      />
+      <div className="my-8 flex gap-4">
+        {availableColors.map(color => (
+          <button key={color} onClick={() => handleClick(color)}>
+            {color}
+          </button>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {result === false && <p className="text-red-500">Wrong color!</p>}
+      {result === true && <p className="text-green-500">Correct!</p>}
+    </main>
+  );
 }
 
-export default App
+export default App;
